43ed7f61339bb622db7197306c3418ea28791883
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-03-12 14:29:07 +0530

    v3

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,15 @@
-pip install -r requirements.txt 
-python manage.py collectstatic
+#!/bin/bash
+
+# Ensure the script stops if any command fails
+set -e
+
+# Use the correct Python version installed by Vercel
+export VIRTUAL_ENV=$HOME/venv
+export PATH="$VIRTUAL_ENV/bin:$PATH"
+
+# Install dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput
